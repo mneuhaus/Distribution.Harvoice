340f0288e463ed5feb08157683020bd19b28383f
Marc Neuhaus <mneuhaus@famelo.com>
2014-08-05 10:41:28 +0200

    Harvoi sprint1

---
+++ Configuration/PackageStates.php
@@ -0,0 +1,481 @@
+<?php
+# PackageStates.php
+
+# This file is maintained by Flow's package management. Although you can edit it
+# manually, you should rather use the command line commands for maintaining packages.
+# You'll find detailed information about the typo3.flow:package:* commands in their
+# respective help screens.
+
+# This file will be regenerated automatically if it doesn't exist. Deleting this file
+# should, however, never become necessary if you use the package commands.
+
+return array (
+  'packages' => 
+  array (
+    'TYPO3.Fluid' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'typo3/fluid',
+      'state' => 'active',
+      'packagePath' => 'Framework/TYPO3.Fluid/',
+      'classesPath' => 'Classes/',
+    ),
+    'TYPO3.Eel' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'typo3/eel',
+      'state' => 'active',
+      'packagePath' => 'Framework/TYPO3.Eel/',
+      'classesPath' => 'Classes/',
+    ),
+    'doctrine.inflector' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/inflector',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/inflector/',
+      'classesPath' => 'lib/',
+    ),
+    'doctrine.cache' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/cache',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/cache/',
+      'classesPath' => 'lib/',
+    ),
+    'doctrine.collections' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/collections',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/collections/',
+      'classesPath' => 'lib/',
+    ),
+    'doctrine.lexer' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/lexer',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/lexer/',
+      'classesPath' => 'lib/',
+    ),
+    'doctrine.annotations' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/annotations',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/annotations/',
+      'classesPath' => 'lib/',
+    ),
+    'Doctrine.Common' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/common',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/common/',
+      'classesPath' => 'lib/',
+    ),
+    'Doctrine.DBAL' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/dbal',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/dbal/',
+      'classesPath' => 'lib/',
+    ),
+    'symfony.console' => 
+    array (
+      'manifestPath' => 'Symfony/Component/Console/',
+      'composerName' => 'symfony/console',
+      'state' => 'active',
+      'packagePath' => 'Libraries/symfony/console/',
+      'classesPath' => '',
+    ),
+    'Doctrine.ORM' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/orm',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/orm/',
+      'classesPath' => 'lib/',
+    ),
+    'doctrine.migrations' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/migrations',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/migrations/',
+      'classesPath' => 'lib/',
+    ),
+    'symfony.yaml' => 
+    array (
+      'manifestPath' => 'Symfony/Component/Yaml/',
+      'composerName' => 'symfony/yaml',
+      'state' => 'active',
+      'packagePath' => 'Libraries/symfony/yaml/',
+      'classesPath' => '',
+    ),
+    'symfony.domcrawler' => 
+    array (
+      'manifestPath' => 'Symfony/Component/DomCrawler/',
+      'composerName' => 'symfony/dom-crawler',
+      'state' => 'active',
+      'packagePath' => 'Libraries/symfony/dom-crawler/',
+      'classesPath' => '',
+    ),
+    'Composer.Installers' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'composer/installers',
+      'state' => 'active',
+      'packagePath' => 'Libraries/composer/installers/',
+      'classesPath' => 'src/',
+    ),
+    'TYPO3.Flow' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'typo3/flow',
+      'state' => 'active',
+      'packagePath' => 'Framework/TYPO3.Flow/',
+      'classesPath' => 'Classes/',
+    ),
+    'TYPO3.Party' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'typo3/party',
+      'state' => 'active',
+      'packagePath' => 'Framework/TYPO3.Party/',
+      'classesPath' => 'Classes/',
+    ),
+    'symfony.eventdispatcher' => 
+    array (
+      'manifestPath' => 'Symfony/Component/EventDispatcher/',
+      'composerName' => 'symfony/event-dispatcher',
+      'state' => 'active',
+      'packagePath' => 'Libraries/symfony/event-dispatcher/',
+      'classesPath' => '',
+    ),
+    'symfony.finder' => 
+    array (
+      'manifestPath' => 'Symfony/Component/Finder/',
+      'composerName' => 'symfony/finder',
+      'state' => 'active',
+      'packagePath' => 'Libraries/symfony/finder/',
+      'classesPath' => '',
+    ),
+    'DavidBadura.Fixtures' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'davidbadura/fixtures',
+      'state' => 'active',
+      'packagePath' => 'Libraries/davidbadura/fixtures/',
+      'classesPath' => 'src/',
+    ),
+    'Famelo.Common' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'famelo/common',
+      'state' => 'active',
+      'packagePath' => 'Application/Famelo.Common/',
+      'classesPath' => 'Classes/',
+    ),
+    'nikic.phpparser' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'nikic/php-parser',
+      'state' => 'active',
+      'packagePath' => 'Libraries/nikic/php-parser/',
+      'classesPath' => 'Classes/',
+    ),
+    'TYPO3.Kickstart' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'typo3/kickstart',
+      'state' => 'active',
+      'packagePath' => 'Framework/TYPO3.Kickstart/',
+      'classesPath' => 'Classes/',
+    ),
+    'famelo.stringscore' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'famelo/string-score',
+      'state' => 'active',
+      'packagePath' => 'Libraries/famelo/string-score/',
+      'classesPath' => 'Classes/',
+    ),
+    'Famelo.Bean' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'famelo/bean',
+      'state' => 'active',
+      'packagePath' => 'Application/Famelo.Bean/',
+      'classesPath' => 'Classes/',
+    ),
+    'Famelo.Bootstrap' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'famelo/bootstrap',
+      'state' => 'active',
+      'packagePath' => 'Application/Famelo.Bootstrap/',
+      'classesPath' => 'Classes/',
+    ),
+    'knplabs.knpsnappy' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'knplabs/knp-snappy',
+      'state' => 'active',
+      'packagePath' => 'Libraries/knplabs/knp-snappy/',
+      'classesPath' => 'src/',
+    ),
+    'Famelo.Harvest' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'famelo/harvest',
+      'state' => 'active',
+      'packagePath' => 'Application/Famelo.Harvest/',
+      'classesPath' => 'Classes/',
+    ),
+    'swiftmailer.swiftmailer' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'swiftmailer/swiftmailer',
+      'state' => 'active',
+      'packagePath' => 'Libraries/swiftmailer/swiftmailer/',
+      'classesPath' => 'Classes/',
+    ),
+    'TYPO3.SwiftMailer' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'typo3/swiftmailer',
+      'state' => 'active',
+      'packagePath' => 'Application/TYPO3.SwiftMailer/',
+      'classesPath' => 'Classes/',
+    ),
+    'Famelo.Messaging' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'famelo/messaging',
+      'state' => 'active',
+      'packagePath' => 'Application/Famelo.Messaging/',
+      'classesPath' => 'Classes/',
+    ),
+    'Famelo.PDF' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'famelo/pdf',
+      'state' => 'active',
+      'packagePath' => 'Application/Famelo.PDF/',
+      'classesPath' => 'Classes/',
+    ),
+    'Flowpack.Expose' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'flowpack/expose',
+      'state' => 'active',
+      'packagePath' => 'Application/Flowpack.Expose/',
+      'classesPath' => 'Classes/',
+    ),
+    'mikey179.vfsStream' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'mikey179/vfsStream',
+      'state' => 'active',
+      'packagePath' => 'Libraries/mikey179/vfsStream/',
+      'classesPath' => 'src/main/php/',
+    ),
+    'phpunit.phpfileiterator' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'phpunit/php-file-iterator',
+      'state' => 'active',
+      'packagePath' => 'Libraries/phpunit/php-file-iterator/',
+      'classesPath' => 'Classes/',
+    ),
+    'phpunit.phptokenstream' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'phpunit/php-token-stream',
+      'state' => 'active',
+      'packagePath' => 'Libraries/phpunit/php-token-stream/',
+      'classesPath' => 'Classes/',
+    ),
+    'phpunit.phptexttemplate' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'phpunit/php-text-template',
+      'state' => 'active',
+      'packagePath' => 'Libraries/phpunit/php-text-template/',
+      'classesPath' => 'Classes/',
+    ),
+    'phpunit.phpcodecoverage' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'phpunit/php-code-coverage',
+      'state' => 'active',
+      'packagePath' => 'Libraries/phpunit/php-code-coverage/',
+      'classesPath' => 'Classes/',
+    ),
+    'phpunit.phptimer' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'phpunit/php-timer',
+      'state' => 'active',
+      'packagePath' => 'Libraries/phpunit/php-timer/',
+      'classesPath' => 'Classes/',
+    ),
+    'phpunit.phpunitmockobjects' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'phpunit/phpunit-mock-objects',
+      'state' => 'active',
+      'packagePath' => 'Libraries/phpunit/phpunit-mock-objects/',
+      'classesPath' => 'Classes/',
+    ),
+    'phpunit.phpunit' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'phpunit/phpunit',
+      'state' => 'active',
+      'packagePath' => 'Libraries/phpunit/phpunit/',
+      'classesPath' => 'Classes/',
+    ),
+    'lusitanian.oauth' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'lusitanian/oauth',
+      'state' => 'active',
+      'packagePath' => 'Libraries/lusitanian/oauth/',
+      'classesPath' => 'src/',
+    ),
+    'oryzone.oauthuserdata' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'oryzone/oauth-user-data',
+      'state' => 'active',
+      'packagePath' => 'Libraries/oryzone/oauth-user-data/',
+      'classesPath' => 'src/OAuth/UserData/',
+    ),
+    'justinrainbow.jsonschema' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'justinrainbow/json-schema',
+      'state' => 'active',
+      'packagePath' => 'Libraries/justinrainbow/json-schema/',
+      'classesPath' => 'src/',
+    ),
+    'alecsammon.phpramlparser' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'alecsammon/php-raml-parser',
+      'state' => 'active',
+      'packagePath' => 'Libraries/alecsammon/php-raml-parser/',
+      'classesPath' => 'src/',
+    ),
+    'Famelo.Oauth' => 
+    array (
+      'state' => 'active',
+      'packagePath' => 'Application/Famelo.Oauth/',
+      'classesPath' => 'Classes/',
+      'manifestPath' => '',
+      'composerName' => 'famelo/oauth',
+    ),
+    'Guzzle.Common' => 
+    array (
+      'manifestPath' => 'Guzzle/Common/',
+      'composerName' => 'guzzle/common',
+      'state' => 'active',
+      'packagePath' => 'Libraries/guzzle/common/',
+      'classesPath' => '',
+    ),
+    'Guzzle.Parser' => 
+    array (
+      'manifestPath' => 'Guzzle/Parser/',
+      'composerName' => 'guzzle/parser',
+      'state' => 'active',
+      'packagePath' => 'Libraries/guzzle/parser/',
+      'classesPath' => '',
+    ),
+    'Guzzle.Stream' => 
+    array (
+      'manifestPath' => 'Guzzle/Stream/',
+      'composerName' => 'guzzle/stream',
+      'state' => 'active',
+      'packagePath' => 'Libraries/guzzle/stream/',
+      'classesPath' => '',
+    ),
+    'Guzzle.Http' => 
+    array (
+      'manifestPath' => 'Guzzle/Http/',
+      'composerName' => 'guzzle/http',
+      'state' => 'active',
+      'packagePath' => 'Libraries/guzzle/http/',
+      'classesPath' => '',
+    ),
+    'symfony.httpfoundation' => 
+    array (
+      'manifestPath' => 'Symfony/Component/HttpFoundation/',
+      'composerName' => 'symfony/http-foundation',
+      'state' => 'active',
+      'packagePath' => 'Libraries/symfony/http-foundation/',
+      'classesPath' => '',
+    ),
+    'Omnipay.Common' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'omnipay/common',
+      'state' => 'active',
+      'packagePath' => 'Libraries/omnipay/common/',
+      'classesPath' => 'src/',
+    ),
+    'omnipay.manual' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'omnipay/manual',
+      'state' => 'active',
+      'packagePath' => 'Libraries/omnipay/manual/',
+      'classesPath' => 'src/',
+    ),
+    'Famelo.Saas' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'famelo/saas',
+      'state' => 'active',
+      'packagePath' => 'Application/Famelo.Saas/',
+      'classesPath' => 'Classes/',
+    ),
+    'Flowpack.Branches' => 
+    array (
+      'state' => 'active',
+      'packagePath' => 'Application/Flowpack.Branches/',
+      'classesPath' => 'Classes/',
+      'manifestPath' => '',
+      'composerName' => 'flowpack/branches',
+    ),
+    'Flowpack.BranchesExtension' => 
+    array (
+      'state' => 'active',
+      'packagePath' => 'Application/Flowpack.BranchesExtension/',
+      'classesPath' => 'Classes/',
+      'manifestPath' => '',
+      'composerName' => 'flowpack/branchesextension',
+    ),
+    'Famelo.Surf.SharedHosting' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'famelo/surf-sharedhosting',
+      'state' => 'active',
+      'packagePath' => 'Application/Famelo.Surf.SharedHosting/',
+      'classesPath' => 'Classes/',
+    ),
+    'TYPO3.Surf' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'typo3/surf',
+      'state' => 'active',
+      'packagePath' => 'Application/TYPO3.Surf/',
+      'classesPath' => 'Classes/',
+    ),
+  ),
+  'version' => 4,
+);
