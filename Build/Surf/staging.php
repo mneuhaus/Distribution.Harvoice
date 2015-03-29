1425eab930ce9c2ab4689405bd3a7d3417da565d
Marc Neuhaus <mneuhaus@famelo.com>
2015-03-29 23:37:24 +0200

    updated dependencies and staging deployment

--- Build/Surf/staging.php
+++ Build/Surf/staging.php
@@ -5,7 +5,7 @@ use \TYPO3\Surf\Domain\Model\SimpleWorkflow;
 
 $application = new \Famelo\Surf\SharedHosting\Application\Flow();
 $application->setOption('repositoryUrl', 'https://mneuhaus@github.com/mneuhaus/Distribution.Harvoice.git');
-$application->setDeploymentPath('/html/staging/');
+$application->setDeploymentPath('/html/staging');
 $application->setOption('keepReleases', 3);
 
 $application->setOption('defaultContext', 'Production');
@@ -21,12 +21,11 @@ $deployment->addApplication($application);
 $workflow = new SimpleWorkflow();
 $workflow->setEnableRollback(FALSE);
 
-// $workflow->addTask('famelo.surf.sharedhosting:beardpatch', 'package');
-// $workflow
-// 	->afterTask('typo3.surf:composer:install', array(
-// 		'famelo.surf.sharedhosting:downloadbeard',
-// 		'famelo.surf.sharedhosting:beardpatch'
-// 	), $application);
+$workflow
+	->afterTask('typo3.surf:typo3:flow:copyconfiguration', array(
+		'famelo.surf.sharedhosting:downloadbeard',
+		'famelo.surf.sharedhosting:beardpatch'
+	), $application);
 
 $deployment->setWorkflow($workflow);
 
